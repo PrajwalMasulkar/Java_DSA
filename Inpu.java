e2affeab8164614c70961d401608f26e7fab0317
PrajwalMasulkar <prajwal19102@iiitnr.edu.in>
2022-02-13 09:59:38 +0530

    input

--- Inpu.java
+++ Inpu.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class Inpu{
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        System.out.println("Your input was "+n);
+       Scanner scn = new Scanner(System.in);
+        /*int n = scn.nextInt();
+        System.out.println("Your input was "+n);*/
+        String s = scn.nextLine();
+        System.out.println(s);
     }
 }
