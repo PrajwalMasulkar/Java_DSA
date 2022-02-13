0990fa69d483af651951319abffeb8f8089cda82
PrajwalMasulkar <prajwal19102@iiitnr.edu.in>
2022-02-13 17:13:07 +0530

    var

---
+++ getting-started/Var.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Var {
+    public static void main(String[] args) throws Exception {
+        /*System.out.println(Math.pow(2, 3));*/
+        int exp = (int)Math.pow(2, 3);
+        System.out.println(exp);
+    }
+}
